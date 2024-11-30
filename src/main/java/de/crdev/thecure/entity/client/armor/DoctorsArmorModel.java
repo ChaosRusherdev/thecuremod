96452bb971805d77d0830633a499e2a9b1080f22
ChaosRusherdev <cr.dev404@gmail.com>
2024-11-30 18:23:35 +0100

    added potion gauntlet

---
+++ src/main/java/de/crdev/thecure/entity/client/armor/DoctorsArmorModel.java
@@ -0,0 +1,23 @@
+package de.crdev.thecure.entity.client.armor;
+
+import de.crdev.thecure.TheCureMod;
+import de.crdev.thecure.item.custom.DoctorsArmorItem;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class DoctorsArmorModel extends AnimatedGeoModel<DoctorsArmorItem> {
+    @Override
+    public Identifier getModelResource(DoctorsArmorItem object) {
+        return new Identifier(TheCureMod.MOD_ID, "geo/doctors_armor.geo.json");
+    }
+
+    @Override
+    public Identifier getTextureResource(DoctorsArmorItem object) {
+        return new Identifier(TheCureMod.MOD_ID, "textures/models/armor/doctors_armor.png");
+    }
+
+    @Override
+    public Identifier getAnimationResource(DoctorsArmorItem animatable) {
+        return new Identifier(TheCureMod.MOD_ID, "animations/doctors_armor.animation.json");
+    }
+}
---
+++ src/main/java/de/crdev/thecure/item/custom/ModArmorItem.java
@@ -0,0 +1,11 @@
+package de.crdev.thecure.item.custom;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class ModArmorItem extends ArmorItem {
+    public ModArmorItem(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+        super(material, slot, settings);
+    }
+}
