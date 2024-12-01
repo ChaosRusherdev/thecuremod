bfabea7dae367266e255bac58ed5c8daecbc2219
ChaosRusherdev <cr.dev404@gmail.com>
2024-12-01 19:05:15 +0100

    added cooldown to the PotionGauntletItem

--- src/main/java/de/crdev/thecure/item/custom/PotionGauntletItem.java
+++ src/main/java/de/crdev/thecure/item/custom/PotionGauntletItem.java
@@ -21,19 +21,19 @@ public class PotionGauntletItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-        if (!world.isClient) {
-            SculcAcidJarProjectileEntity sculcAcidProjectileVialEntity = new SculcAcidJarProjectileEntity(user, world);
-            sculcAcidProjectileVialEntity.setItem(ModItems.SCULC_ACID_JAR.getDefaultStack());
-            sculcAcidProjectileVialEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+        user.getItemCooldownManager().set(this, 20);
 
-            world.spawnEntity(sculcAcidProjectileVialEntity);
-        }
+        if (!world.isClient) {
+                SculcAcidJarProjectileEntity sculcAcidProjectileVialEntity = new SculcAcidJarProjectileEntity(user, world);
+                sculcAcidProjectileVialEntity.setItem(ModItems.SCULC_ACID_JAR.getDefaultStack());
+                sculcAcidProjectileVialEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+                world.spawnEntity(sculcAcidProjectileVialEntity);
+            }
 
         user.incrementStat(Stats.USED.getOrCreateStat(this));
         if (!user.getAbilities().creativeMode) {
             itemStack.decrement(1);
         }
-
         return TypedActionResult.success(itemStack, world.isClient());
     }
 
