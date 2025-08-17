85beeeffb2ab4080bc61019945b20ff6d42ed773
ChaosRusherdev <cr.dev404@gmail.com>
2025-08-18 00:34:52 +0200

    fourth part migration to 1.21

--- src/main/java/de/crdev/thecure/item/custom/DoctorsArmorItem.java
+++ src/main/java/de/crdev/thecure/item/custom/DoctorsArmorItem.java
@@ -1,71 +1,41 @@
 package de.crdev.thecure.item.custom;
 
-import de.crdev.thecure.item.client.DoctorsArmorRenderer;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.entity.AnimationState;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
 import software.bernie.geckolib.animatable.GeoItem;
-import software.bernie.geckolib.animatable.client.RenderProvider;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
-import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.*;
-import software.bernie.geckolib.core.object.PlayState;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+public final class DoctorsArmorItem extends ArmorItem implements GeoItem {
 
-public class DoctorsArmorItem extends ArmorItem implements GeoItem {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    private final Supplier<Object> renderProvider = GeoItem.makeRenderer(this);
-    public DoctorsArmorItem(ArmorMaterial material, Type type, Settings settings) {
+
+    public DoctorsArmorItem(RegistryEntry<ArmorMaterial> material, Type type, Settings settings) {
         super(material, type, settings);
     }
 
     @Override
-    public void createRenderer(Consumer<Object> consumer) {
-        consumer.accept(new RenderProvider() {
-            private DoctorsArmorRenderer renderer;
-
-
-            @Override
-            public BipedEntityModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack,
-                                                                        EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original) {
-                if (this.renderer == null)
-                    this.renderer = new DoctorsArmorRenderer();
-
-
-                this.renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
-
-
-                return this.renderer;
-            }
-        });
-    }
-
-
-    @Override
-    public Supplier<Object> getRenderProvider() {
-        return this.renderProvider;
-    }
-
-    @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController(this,"controller", 0, this::predicate));
-    }
+        controllers.add(new AnimationController<>(this, 20, state -> {
+            // Apply our generic idle animation.
+            // Whether it plays or not is decided down below.
+            state.getController().setAnimation(DefaultAnimations.IDLE);
 
-    private PlayState predicate(AnimationState animationState) {
-        animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
-        return PlayState.CONTINUE;
-    }
+            // Let's gather some data from the state to use below
+            // This is the entity that is currently wearing/holding the item
+            Entity entity = state.getData(DataTickets.ENTITY);
 
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+            // Play the animation if the full set is being worn, otherwise stop
 
+            return PlayState.CONTINUE;
+        }));
     }
 
     @Override
